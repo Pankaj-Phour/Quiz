658c79e50de3933d827d95240f1ac6b00b29b09e
Pankaj Phour <pankaj.phour70@gmail.com>
2023-06-05 08:53:34 +0530

    notification component added

---
+++ src/app/notify/notify.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notify',
+  templateUrl: './notify.component.html',
+  styleUrls: ['./notify.component.scss']
+})
+export class NotifyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
